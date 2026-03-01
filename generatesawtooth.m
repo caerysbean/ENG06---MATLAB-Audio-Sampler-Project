8e3bf3645a421fb1a7f5891a43631b941731021d
Caerys Bean <143555302+caerysbean@users.noreply.github.com>
2026-02-28 21:36:41 -0800

    Modify generateSawtooth function to output sawtoothFunction
    Added output for sawtoothFunction for the Synthesizer on the GUI

--- generatesawtooth.m
+++ generatesawtooth.m
@@ -1,7 +1,7 @@
-function sawtoothSample = generateSawtooth(afreq)
+function [sawtoothSample, sawtoothFunction] = generateSawtooth(afreq)
 fs = 44100;
 %afreq = 261.63; %C4 sample
-t = 0:1/fs:5; % Time vector for 5 second duration
+t = 0:1/fs:0.5; % Time vector for 5 second duration
 sawtoothFunction = (sawtooth(2*pi*afreq*t))'; %Given as a column vector
 % Play the generated sine wave sound
 sawtoothSample = audioplayer(sawtoothFunction, fs);
