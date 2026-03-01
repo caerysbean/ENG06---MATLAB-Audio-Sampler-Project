4a25fc4ce9589e595cc89e6becccfcb12fab930d
Caerys Bean <143555302+caerysbean@users.noreply.github.com>
2026-02-28 21:34:33 -0800

    Refactor generateSine function to return sineWave
    Fixed function to return sine wave for plotting on Synthesizer

--- generatesine.m
+++ generatesine.m
@@ -1,13 +1,13 @@
-function sineSample = generateSine(afreq)
+function [sineSample, sineWave] = generateSine(afreq)
 
 %Sampling Frequency, using the Nyquist Theorem and accounting for effects
 %of DAC low pass filter
 fs = 44100;
 %afreq = 261.63; %C4 sample
-t = 0:1/fs:5; % Time vector for 5 second duration
-sine = (sin(2*pi*afreq*t))'; %Given as a column vector
+t = 0:1/fs:0.5; % Time vector for 5 second duration
+sineWave = (sin(2*pi*afreq*t))'; %Given as a column vector
 % Play the generated sine wave sound
-sineSample = audioplayer(sine, fs);
+sineSample = audioplayer(sineWave, fs);
 %play(sineSample) - implement this in the GUI callback
 
 end 
