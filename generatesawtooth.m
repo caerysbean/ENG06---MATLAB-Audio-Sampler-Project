d8898116beceb698044888cf5ba65b47061e0e7c
Caerys Bean <143555302+caerysbean@users.noreply.github.com>
2026-02-28 19:45:16 -0800

    Updated the sawtooth sample into a function
    Refactor generateSawtooth function to accept frequency as input and remove hardcoded values. Update comments for clarity.

--- generatesawtooth.m
+++ generatesawtooth.m
@@ -1,10 +1,10 @@
-%Sampling Frequency, using the Nyquist Theorem and accounting for effects
-%of DAC low pass filter
-%Using Signal Processing Toolbox for sawtooth
+function sawtoothSample = generateSawtooth(afreq)
 fs = 44100;
-afreq = 261.63; %C4 sample
+%afreq = 261.63; %C4 sample
 t = 0:1/fs:5; % Time vector for 5 second duration
 sawtoothFunction = (sawtooth(2*pi*afreq*t))'; %Given as a column vector
 % Play the generated sine wave sound
 sawtoothSample = audioplayer(sawtoothFunction, fs);
-play(sawtoothSample)
+%play(sawtoothSample) - implement this in the GUI callback
+
+end 
